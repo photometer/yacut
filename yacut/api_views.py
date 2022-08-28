64128fd577077d86c23677e1c79802f14dab6e68
photometer <androsovaelizaveta@mail.ru>
2022-08-28 11:37:44 +0300

    Project is ready for review

---
+++ yacut/api_views.py
@@ -0,0 +1,39 @@
+from re import fullmatch
+
+from flask import jsonify, request
+
+from . import app, db
+from .error_handlers import InvalidAPIUsage
+from .models import URL_map
+from .utils import get_unique_short_id
+
+
+@app.route('/api/id/', methods=['POST'])
+def create_url():
+    PATTERN = r'^[a-zA-Z0-9]{1,16}$'
+
+    data = request.get_json()
+    if not data:
+        raise InvalidAPIUsage('Отсутствует тело запроса')
+    if not data.get('url'):
+        raise InvalidAPIUsage('"url" является обязательным полем!')
+    short_id = data.get('custom_id')
+    if not short_id:
+        data['custom_id'] = get_unique_short_id()
+    elif URL_map.query.filter_by(short=short_id).first():
+        raise InvalidAPIUsage(f'Имя "{short_id}" уже занято.')
+    elif not fullmatch(PATTERN, short_id):
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
+    url_map = URL_map()
+    url_map.from_dict(data)
+    db.session.add(url_map)
+    db.session.commit()
+    return jsonify(url_map.to_dict()), 201
+
+
+@app.route('/api/id/<string:short_id>/', methods=['GET'])
+def get_url(short_id):
+    url_map = URL_map.query.filter_by(short=short_id).first()
+    if not url_map:
+        raise InvalidAPIUsage('Указанный id не найден', 404)
+    return jsonify(url=url_map.original), 200
